935342890f685757c98ff3a1816f3c3de41b1538
Chris Stasiak <me@krzysztof-stasiak.com>
2016-01-13 12:45:13 +0100

    Improved room destroy logic

--- libjanus_audioroom.c
+++ libjanus_audioroom.c
@@ -888,18 +888,20 @@ void cm_audioroom_destroy_session(janus_plugin_session *handle, int *error) {
 		//	session->rooms = NULL;
 		//}
 
-		cm_audioroom_participant *participant = (cm_audioroom_participant*)session->participant;
-		janus_mutex_lock(&participant->qmutex);
-		if (g_hash_table_size(participant->room->participants) == 1) {
-			cm_audioroom_room_destroy(participant->room, NULL);
-		}
-		janus_mutex_unlock(&participant->qmutex);
+		// Keep reference for session audioroom
+		cm_audioroom_participant *participant = (cm_audioroom_participant *)session->participant;
+		cm_audioroom_room *audioroom = participant->room;
 
 		g_hash_table_remove(sessions, handle);
 		cm_audioroom_hangup_media(handle);
 		session->destroyed = janus_get_monotonic_time();
 		/* Cleaning up and removing the session is done in a lazy way */
 		old_sessions = g_list_append(old_sessions, session);
+
+		// Remove audioroom if not more participants
+		if (g_hash_table_size(audioroom->participants) == 0) {
+			cm_audioroom_room_destroy(audioroom, NULL);
+		}
 	}
 	janus_mutex_unlock(&sessions_mutex);
 
